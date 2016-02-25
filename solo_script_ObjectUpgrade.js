d768725f8088eb5df403eb42b66720fc7422be0a
ekimlinger <ekimlinger@gmail.com>
2016-02-25 17:10:24 -0600

    added all files EXCLUDE SUPEROBJECT

---
+++ solo_script_ObjectUpgrade.js
@@ -0,0 +1,93 @@
+// ! ! !
+// Three Bugs
+
+function Employee(name, id, salary, rating){
+  this.name = name;
+  this.id = id;
+  this.salary = salary;
+  this.rating = rating;
+}
+
+var Atticus = new Employee("Atticus", "2405", "47000", 3);
+var Jem = new Employee("Jem", "62347", "63500", 4);
+var Boo = new Employee("Boo", "11435", "54000", 3);
+var Scout = new Employee("Scout", "6243", "74750", 5);
+
+var array = [Atticus, Jem, Boo, Scout];
+console.log(array);
+
+//Create variables used to write to the DOM
+var newEl, newText, position, STIarray;
+//Capture the position of insertion into the DOM
+position = document.getElementById('content');
+
+//Loop the array, extracting each array and writing information to the DOM
+//Note that the information is not 'clean'
+for(var i = 0; i < array.length; i++){
+
+  STIarray = calculateSTI(array[i]);
+  newEl = document.createElement('li');
+	newText = document.createTextNode(STIarray[i]);
+	newEl.appendChild(newText);
+	position.appendChild(newEl);
+}
+
+function calculateSTI(employee){
+  var newArray = [];
+
+
+  newArray[0] = employee.name;
+  var employeeNumber = employee.id;
+  var baseSalary = employee.salary;
+  var reviewScore = employee.rating;
+
+  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  if(bonus > 0.13){
+    bonus = 0.13;
+  }
+
+  newArray[1] = bonus;
+  newArray[2] = Math.round( baseSalary * (1.0 + bonus) );
+  newArray[3] = Math.round( baseSalary * bonus );
+  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+  return newArray;
+}
+
+function getBaseSTI(reviewScore){
+  var basePercent;
+  switch(reviewScore){
+    case 1:
+      basePercent = 0;
+      break;
+    case 2:
+      basePercent = 0;
+      break;
+    case 3:
+      basePercent = 0.04;
+      break;
+    case 4:
+      basePercent = 0.06;
+      break;
+    case 5:
+      basePercent = 0.10;
+      break;
+  }
+  return basePercent;
+}
+
+function getYearAdjustment(employeeNumber){
+  var yearAdjustment = 0;
+  if(employeeNumber.length == 4){
+    yearAdjustment = 0.05;
+  }
+  return yearAdjustment;
+}
+
+function getIncomeAdjustment(salary){
+  var incomeAdjustment = 0;
+  salary = parseInt(salary);
+  if(salary > 65000){
+    incomeAdjustment = 0.01;
+  }
+  return incomeAdjustment;
+}
